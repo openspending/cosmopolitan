edf4227d7337569846562fd669d3d34c80b44985
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-11-06 21:02:40 +0300

    Show counry and regions
      related to #1

--- world/serializers.py
+++ world/serializers.py
@@ -1,19 +1,23 @@
 from cities.models import Country, Region, AlternativeName
 from rest_framework import serializers
-# from rest_framework_nested.relations import NestedHyperlinkedRelatedField
-
-# Serializers define the API representation.
-class CountrySerializer(serializers.HyperlinkedModelSerializer):
-    regions = serializers.HyperlinkedIdentityField(view_name='region-detail')
-
-    class Meta:
-        model = Country
-
-class AlternativeNameSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = AlternativeName
 
 class RegionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Region
-        fields = ('name', 'slug')
+
+
+class CountrySerializer(serializers.HyperlinkedModelSerializer):
+    regions = serializers.SerializerMethodField('region_url')
+
+    def region_url(self, country):
+        request = self.context.get('request', None)
+
+        return "%s%d/regions" % (request.build_absolute_uri(), country.pk)
+
+    class Meta:
+        model = Country
+
+
+class AlternativeNameSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = AlternativeName
--- world/views.py
+++ world/views.py
@@ -1,16 +1,23 @@
 from cities.models import Country, Region, AlternativeName
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, generics
+from rest_framework.response import Response
 from .serializers import CountrySerializer, RegionSerializer, AlternativeNameSerializer
 
-# ViewSets define the view behavior.
+
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
 
 class RegionViewSet(viewsets.ModelViewSet):
-    queryset = Region.objects.all()
+    model = Region
     serializer_class = RegionSerializer
+
+    def get_queryset(self):
+        queryset = Region.objects.all()
+        country_id = self.kwargs['country_id']
+        return queryset.filter(country=country_id)
+
 
 class AlternativeNameViewSet(viewsets.ModelViewSet):
     queryset = AlternativeName.objects.all()
