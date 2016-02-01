0199be908bfb9e1a6c7d55f7f4d173a494f9c62c
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-02-01 19:30:31 +0300

    fix settings

--- application/wsgi.py
+++ application/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cosmopolitan.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "application.settings")
 
 application = Cling(get_wsgi_application())
 application = get_wsgi_application()
